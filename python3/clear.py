8780ae6c47be0904c78e27576c6965be26964eaf
ealgase <mostdigitsofpi@gmail.com>
2015-12-11 21:33:13 +0000

    add clear.py

---
+++ python3/clear.py
@@ -0,0 +1,3 @@
+#python3 has no clear command.  this command comes close by printing 100 blank lines.
+def clear():
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
